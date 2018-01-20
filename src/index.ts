6cd837a5b4ce5eab196dd0b47233ec08de32adc7
york yao <yaoao12306@outlook.com>
2018-01-20 09:35:09 +0800

    refactor: use tslint-config-standard as lint rule

--- src/index.ts
+++ src/index.ts
@@ -1,109 +1,109 @@
-import * as minimist from "minimist";
-import * as fs from "fs";
-import * as path from "path";
-import * as util from "util";
-import * as packageJson from "../package.json";
+import * as minimist from 'minimist'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as util from 'util'
+import * as packageJson from '../package.json'
 
-let suppressError = false;
+let suppressError = false
 
-function showToolVersion() {
-    console.log(`Version: ${packageJson.version}`);
+function showToolVersion () {
+  console.log(`Version: ${packageJson.version}`)
 }
 
-async function executeCommandLine() {
-    const argv = minimist(process.argv.slice(2), { "--": true });
+async function executeCommandLine () {
+  const argv = minimist(process.argv.slice(2), { '--': true })
 
-    const showVersion = argv.v || argv.version;
-    if (showVersion) {
-        showToolVersion();
-        return;
+  const showVersion = argv.v || argv.version
+  if (showVersion) {
+    showToolVersion()
+    return
+  }
+
+  suppressError = argv.suppressError
+
+  const exclude: string | string[] = argv.e || argv.exclude
+  const excludedDirectories: string[] = []
+  if (Array.isArray(exclude)) {
+    for (const e of exclude) {
+      excludedDirectories.push(e)
     }
+  } else if (exclude) {
+    excludedDirectories.push(exclude)
+  }
 
-    suppressError = argv.suppressError;
+  interface Node {
+    name: string
+    isDirectory?: boolean
+    children?: Node[]
+  }
 
-    const exclude: string | string[] = argv.e || argv.exclude;
-    const excludedDirectories: string[] = [];
-    if (Array.isArray(exclude)) {
-        for (const e of exclude) {
-            excludedDirectories.push(e);
+  const statAsync = util.promisify(fs.stat)
+  const readdirAsync = util.promisify(fs.readdir)
+  async function readAsync (node: Node): Promise<boolean> {
+    const directoryName = path.basename(node.name)
+    if (excludedDirectories.some(d => directoryName === d)) {
+      return false
+    }
+    const stats = await statAsync(node.name)
+    if (stats.isFile()) {
+      node.isDirectory = false
+      return true
+    } else if (stats.isDirectory()) {
+      node.isDirectory = true
+      node.children = []
+      const files = await readdirAsync(node.name)
+      for (const file of files) {
+        const child: Node = {
+          name: path.resolve(node.name, file)
         }
-    } else if (exclude) {
-        excludedDirectories.push(exclude);
-    }
-
-    interface Node {
-        name: string;
-        isDirectory?: boolean;
-        children?: Node[];
-    }
-
-    const statAsync = util.promisify(fs.stat);
-    const readdirAsync = util.promisify(fs.readdir);
-    async function readAsync(node: Node): Promise<boolean> {
-        const directoryName = path.basename(node.name);
-        if (excludedDirectories.some(d => directoryName === d)) {
-            return false;
+        const isValid = await readAsync(child)
+        if (isValid) {
+          node.children.push(child)
         }
-        const stats = await statAsync(node.name);
-        if (stats.isFile()) {
-            node.isDirectory = false;
-            return true;
-        } else if (stats.isDirectory()) {
-            node.isDirectory = true;
-            node.children = [];
-            const files = await readdirAsync(node.name);
-            for (const file of files) {
-                const child: Node = {
-                    name: path.resolve(node.name, file),
-                };
-                const isValid = await readAsync(child);
-                if (isValid) {
-                    node.children.push(child);
-                }
-            }
-            return true;
-        }
-        return false;
+      }
+      return true
     }
+    return false
+  }
 
-    const result: Node[] = [];
-    for (const file of argv._) {
-        const basename = path.basename(file);
-        if (excludedDirectories.every(d => basename !== d)) {
-            const node: Node = {
-                name: file,
-            };
-            const isValid = await readAsync(node);
-            if (isValid) {
-                result.push(node);
-            }
-        }
+  const result: Node[] = []
+  for (const file of argv._) {
+    const basename = path.basename(file)
+    if (excludedDirectories.every(d => basename !== d)) {
+      const node: Node = {
+        name: file
+      }
+      const isValid = await readAsync(node)
+      if (isValid) {
+        result.push(node)
+      }
     }
+  }
 
-    function print(node: Node, depth: number, isEnd: boolean) {
-        const head = depth === 0 ? "" : "│  ".repeat(depth - 1) + (isEnd ? "└─ " : "├─ ");
-        console.log(head + path.basename(node.name));
-        if (node.children) {
-            for (let i = 0; i < node.children.length; i++) {
-                print(node.children[i], depth + 1, i === node.children.length - 1);
-            }
-        }
+  function print (node: Node, depth: number, isEnd: boolean) {
+    const head = depth === 0 ? '' : '│  '.repeat(depth - 1) + (isEnd ? '└─ ' : '├─ ')
+    console.log(head + path.basename(node.name))
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i++) {
+        print(node.children[i], depth + 1, i === node.children.length - 1)
+      }
     }
+  }
 
-    for (let i = 0; i < result.length; i++) {
-        print(result[i], 0, i === result.length - 1);
-    }
+  for (let i = 0; i < result.length; i++) {
+    print(result[i], 0, i === result.length - 1)
+  }
 }
 
 executeCommandLine().then(() => {
-    console.log("files2text success.");
+  console.log('files2text success.')
 }, error => {
-    if (error instanceof Error) {
-        console.log(error.message);
-    } else {
-        console.log(error);
-    }
-    if (!suppressError) {
-        process.exit(1);
-    }
-});
+  if (error instanceof Error) {
+    console.log(error.message)
+  } else {
+    console.log(error)
+  }
+  if (!suppressError) {
+    process.exit(1)
+  }
+})
